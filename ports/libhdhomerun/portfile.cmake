7803dde3561cb61a7be1db1e809629af7fa1c401
Taylor Southwick <twsouthwick@outlook.com>
2017-06-13 17:07:21 -0700

    Add libhdhomerun

---
+++ ports/libhdhomerun/portfile.cmake
@@ -0,0 +1,36 @@
+# Common Ambient Variables:
+#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
+#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
+#   CURRENT_PORT DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
+#   PORT                      = current port name (zlib, etc)
+#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
+#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
+#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
+#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
+#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
+#
+
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libhdhomerun-cmake)
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/twsouthwick/libhdhomerun/archive/cmake.zip"
+    FILENAME "cmake.zip"
+    SHA512 46d26a0671a413b71dee9555d1c3f8d9aedf65ff40c7279616520693d035803fcd40d81a77cd53f5a6a185317731421fbe3578f9ad29d7c02c199ea8837b4ab1
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA # Disable this option if project cannot be built with Ninja
+    # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
+    # OPTIONS_RELEASE -DOPTIMIZE=1
+    # OPTIONS_DEBUG -DDEBUGGABLE=1
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libhdhomerun)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/libhdhomerun/LICENSE ${CURRENT_PACKAGES_DIR}/share/libhdhomerun/copyright)
